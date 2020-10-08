e85c60c87d9ee6ab27a3177c80b2e91485193430
Elon Zhang <clearlovezyq@gmail.com>
2020-10-08 04:46:45 -0400

    Yiqu Zhang: Add third person controller and animation controller

--- Assignment02/Assets/ThirdPersonControllerScript.cs
+++ Assignment02/Assets/ThirdPersonControllerScript.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class ThirdPersonControllerScript : MonoBehaviour
 {
 
+    public CharacterController controller;
+
+    public float speed = 2.0f;
+
+    public float turnSmoothTime = 0.1f;
+
+    public Transform cam;
+
+    public Animator animator;
+
+    private float turnSmoothVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +26,30 @@ public class ThirdPersonControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        if (direction.magnitude > 0.1f) {
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg +cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            animator.SetBool("Moving", true);
+
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        } else {
+            animator.SetBool("Moving", false);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            animator.SetBool("Running", true);
+            speed = 5f;
+        } else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            animator.SetBool("Running", false);
+            speed = 2f;
+        }
     }
 }
