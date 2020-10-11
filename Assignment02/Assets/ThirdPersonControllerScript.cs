62947225cf8d4b7d3fb1e1db3fb4c36b39c4380d
Elon Zhang <clearlovezyq@gmail.com>
2020-10-10 22:15:20 -0400

    Yiqu Zhang: Fix the mouse click moving function, when player clicked the ground, player could move to the destination position without mouse click

--- Assignment02/Assets/ThirdPersonControllerScript.cs
+++ Assignment02/Assets/ThirdPersonControllerScript.cs
@@ -5,25 +5,15 @@ using UnityEngine;
 public class ThirdPersonControllerScript : MonoBehaviour
 {
 
-    // public CharacterController controller;
-
     public float speed = 2.0f;
 
-    // public float turnSmoothTime = 0.1f;
-
-    // public Transform cam;
-
     public Animator animator;
-
-    // it is the ground of the playerbody
-
-    // private float turnSmoothVelocity;
-
-    private Vector3 mouseClickedPosition;
 
     private RaycastHit raycastHit;
 
     private Vector3 storedClickedPosition;
+
+    private string GROUND = "Ground";
 
     // Start is called before the first frame update
     void Start()
@@ -34,70 +24,100 @@ public class ThirdPersonControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // float horizontal = Input.GetAxis("Horizontal");
-        // float vertical = Input.GetAxis("Vertical");
-
-        // Vector3 direction = new Vector3(horizontal, 0f, vertical);
-
-        // if (direction.magnitude > 0.1f) {
-
-        //    float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg +cam.eulerAngles.y;
-        //    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-        //    transform.rotation = Quaternion.Euler(0f, angle, 0f);
-        //    Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-        //    animator.SetBool("Moving", true);
-
-        //    controller.Move(moveDir.normalized * speed * Time.deltaTime);
-        // } else {
-        //    animator.SetBool("Moving", false);
-        // }
-        // if (Input.GetKeyDown(KeyCode.LeftShift)) {
-        //    animator.SetBool("Running", true);
-        //    speed = 5f;
-        // } else if (Input.GetKeyUp(KeyCode.LeftShift)) {
-        //    animator.SetBool("Running", false);
-        //    speed = 2f;
-        // }
-        /**
-         * When click the left mouse, get the current position
-         * Use the current Position to take a raycastHit to make sure it clicked the ground
-         * TODO this method need to be fixed because the third person camera could not work correctly.
-         *
+        /*
          * Another thinking:
          * Auto find path
          * Store the click position, check current position and click position
          * if they do not have same x, z position, player should move to the click position contiune.
          * if the have same x,z position, set the click position null, and stop moving
-         * if player does not move finish but player has a new clicked position, the player should move to the new position
-         * 
+         * if player does not move finish but player has a new clicked position, 
+         *   the player should move to the new position 
+         *   
+         *   
+         *  Auto find path function
+         *
+         *  different situations has different choices
+         *  1.if mouse clicked the ground, store the position to storedClickedPosition and player move to this position.
+         *      during this moving, if player does not move to the expectant position without new click, 
+         *      player should move to the expectant position go on
+         *  2.if mouse clicked the ground and player has not move to the expectant position, 
+         *      store the new position and player should move to the new position
+         *  3.if player has moved to the expectant position, should remove the stored position and stop moving
          */
+
         if (Input.GetMouseButton(0)) {
-            // get the current screen position
+            
+            // get current mouse screen position
             Vector3 currentScreenPosition = Input.mousePosition;
 
-            // declear a ray to screen position 
+            // declear a ray to screen position
             Ray groundCheckRay = Camera.main.ScreenPointToRay(currentScreenPosition);
 
-            // if the ray touch to the ground
+             // if the ray touch to the ground
             if (Physics.Raycast(groundCheckRay, out raycastHit)) {
                 GameObject currentBlockedObject = raycastHit.collider.gameObject;
-                if (currentBlockedObject.tag == "Ground")
-                {
-                    // the ray touched to the ground, so we could get the position that mouse clicked
-                    mouseClickedPosition = raycastHit.point;
-                    // if (storedClickedPosition == Vector3.zero) {
-                    //     storedClickedPosition = new Vector3(mouseClickedPosition.x, mouseClickedPosition.y, mouseClickedPosition.z);
-                    // }
-                    // compare the position
-                    // TODO
-                    transform.LookAt(mouseClickedPosition);
-                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
-                    animator.SetBool("Moving", true);
-                }
+                if (currentBlockedObject.tag == GROUND) {
+                    Vector3 mouseClickedPosition = raycastHit.point;
+                    StoreDestinationPosition(mouseClickedPosition);
+                    Moving2Destination(mouseClickedPosition);
+                }   
             }
         } else {
-            animator.SetBool("Moving", false);
+            if (storedClickedPosition != Vector3.zero) {
+                Moving2Destination(storedClickedPosition);
+            } else {
+                animator.SetBool("Moving", false);
+            }
         }
+        RunOrWalk();
+        if (isArrived2DestinationPosition()) {
+            RemoveDestinationPosition();
+        }
+    }
+
+    // if push left shift, player should run
+    private void RunOrWalk() {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+           animator.SetBool("Running", true);
+           speed = 5f;
+        } else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+           animator.SetBool("Running", false);
+           speed = 2f;
+        }
+    }
+
+    // moving function
+    private void Moving2Destination(Vector3 destinationPosition) {
+
+        transform.LookAt(destinationPosition);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        animator.SetBool("Moving", true);
+    }
+
+    // check the current player whether arrived to the destination position
+    private bool isArrived2DestinationPosition() {
+
+        // should use approximate position because the step will not follow the position, it will have 0-1 error
+        // if (transform.position.x == storedClickedPosition.x && transform.position.z == storedClickedPosition.z) {
+        //     return true;
+        // }
+        // use distance to check the position
+        float destinationDistance = Vector3.Distance(transform.position, storedClickedPosition);
+        if (destinationDistance < 1f) {
+            return true;
+        }
+        return false;
+    }
+
+    // store the destination position
+    private void StoreDestinationPosition(Vector3 destinationPosition) {
+
+        storedClickedPosition = new Vector3(destinationPosition.x, destinationPosition.y, destinationPosition.z);
+    }
+
+    // remove the stored position
+    private void RemoveDestinationPosition() {
+
+        storedClickedPosition = Vector3.zero;
     }
 }
