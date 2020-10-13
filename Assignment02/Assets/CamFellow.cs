b88ab731cfabf2c80c283483d5f5807ae6139a22
Elon Zhang <clearlovezyq@gmail.com>
2020-10-13 04:24:46 -0400

    Yiqu Zhang: Solve the conflict

---
+++ Assignment02/Assets/CamFellow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFellow : MonoBehaviour
+{
+    private Vector3 offset;
+    public Transform player;
+ // Start is called before the first frame update
+    void Start()
+    {
+        offset = player.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         transform.position = Vector3.Lerp(transform.position, player.position - offset,Time.deltaTime*5);
+    }
+}
